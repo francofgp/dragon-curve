0cc551da7f5055ef737cad6d13612a4f419f268c
Giuliano Pértile <giulianopertile@gmail.com>
2022-06-29 12:11:52 -0300

    Finish dragon curve
    Remove jupyter notebook and rich library

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
-from dragon import dragon
+from dragon import dragon, get_r
+from turtle import Turtle, Screen
 
-print(dragon(2))
+# Setup turtle
+turtle = Turtle("turtle")
+turtle.hideturtle()
+turtle.speed(0)
+turtle.color("#ff69aa")
+
+# Setup Screen
+screen = Screen()
+screen.title("Dragon Curve")
+screen.bgcolor("black")
+screen.screensize(1920*3, 1080*3)
+screen.setup(width=1.0, height=1.0, startx=None, starty=None)
+
+
+# Draw
+LENGHT = 12
+for element in dragon(15):
+    if element == get_r():
+        turtle.right(90)
+        turtle.forward(LENGHT)
+        # if you want circles use the line below instead
+        # turtle.circle(-4, 90, 36)
+    else:
+        turtle.left(90)
+        turtle.forward(LENGHT)
+        # if you want circles use the line below instead
+        # turtle.circle(4, 90, 36)
+
+# When finished, click to exit
+turtle.color("white")
+turtle.write("click to exit", font=("Calibri", 16, "bold"))
+screen.exitonclick()
--- dragon.py
+++ dragon.py
@@ -1,19 +1,35 @@
-""" Each dragon curve iteration can be found by copying the previous iteration, 
-    then an R, then a second copy of the previous iteration 
-    in reverse order with the L and R letters swapped.[1]
-
-    https://en.wikipedia.org/wiki/Dragon_curve
-"""
 R = "R"
 L = "L"
 
 
-def iterate(sequence: str):
+def iterate(sequence: str) -> str:
+    """
+    Takes in a dragon curve sequence n, and returns sequence n+1, 
+    i.e.: 
+        when n= "R", returns "RRL"
+
+    Each dragon curve iteration can be found by copying the previous iteration, 
+    then an R, then a second copy of the previous iteration 
+    in reverse order with the L and R letters swapped.
+
+    Source: https://en.wikipedia.org/wiki/Dragon_curve
+
+    Args:
+        sequence (str): dragon curve sequence, formatted "RRL" for sequence 2 of the dragon curve
+
+    Returns:
+        str: sequence n+1
+    """
     sequence = sequence+R+swapLetters(sequence[::-1])
     return sequence
 
 
-def swapLetters(sequence: str):
+def swapLetters(sequence: str) -> str:
+    """
+    Swap a dragon curve sequence. 
+    i.e.:
+     when sequence "RRL", returns "LLR"
+    """
     newSequence = ""
     for letter in sequence:
         if letter == R:
@@ -24,7 +40,34 @@ def swapLetters(sequence: str):
 
 
 def dragon(n_iterations: int) -> str:
+    """Takes in a number n, an return the dragon curve sequence i.e.:
+    When n=2, returns "RRL"
+
+    Args:
+        n_iterations (int): number of iterations of the dragon curve
+
+    Returns:
+        str: The dragon curve Sequence
+    """
     initial_sequence = R
     for i in range(0, n_iterations):
         initial_sequence = iterate(initial_sequence)
     return initial_sequence
+
+
+def get_r() -> str:
+    """Get the string representation of the right turn, which is "R"
+
+    Returns:
+        str: "R"
+    """
+    return R
+
+
+def get_l() -> str:
+    """Get the string representation of the left turn, which is "L"
+
+    Returns:
+        str: "L"
+    """
+    return L
