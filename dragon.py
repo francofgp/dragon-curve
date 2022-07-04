5b9c686ee00352d3d2e5b75e08fbad81d51f10ce
Julien Palard <julien@palard.fr>
2022-07-04 22:54:39 +0200

    Trying to enhance perfs a bit.
    Before, dargon(24) was taking ~7.5s, it now takes ~1.5s.

--- dragon.py
+++ dragon.py
@@ -1,15 +1,15 @@
-R = "R"
-L = "L"
+R = 0
+L = 1
 
 
-def iterate(sequence: str) -> str:
+def next_steps(sequence: list[int]) -> list[int]:
     """
-    Takes in a dragon curve sequence n, and returns sequence n+1, 
-    i.e.: 
-        when n= "R", returns "RRL"
+    Takes in a dragon curve sequence n, and returns the next items up to sequence n+1,
+    i.e.:
+        when sequence=[R], returns [R, L]
 
-    Each dragon curve iteration can be found by copying the previous iteration, 
-    then an R, then a second copy of the previous iteration 
+    Each dragon curve iteration can be found by copying the previous iteration,
+    then an R, then a second copy of the previous iteration
     in reverse order with the L and R letters swapped.
 
     Source: https://en.wikipedia.org/wiki/Dragon_curve
@@ -20,36 +20,29 @@ def iterate(sequence: str) -> str:
     Returns:
         str: sequence n+1
     """
-    sequence = sequence+R+swapLetters(sequence[::-1])
-    return sequence
+    return [R] + swap(reversed(sequence))
 
 
-def swapLetters(sequence: str) -> str:
+def swap(sequence: list[int]) -> list[int]:
     """
-    Swap a dragon curve sequence. 
+    Swap a dragon curve sequence.
     i.e.:
-     when sequence "RRL", returns "LLR"
+     when sequence [R, R, L], returns [L, L, R]
     """
-    newSequence = ""
-    for letter in sequence:
-        if letter == R:
-            newSequence = newSequence + L
-        else:
-            newSequence = newSequence + R
-    return newSequence
+    return [1 - letter for letter in sequence]
 
 
-def dragon(n_iterations: int) -> str:
+def dragon(n_iterations: int) -> list[int]:
     """Takes in a number n, an return the dragon curve sequence i.e.:
-    When n=2, returns "RRL"
+    When n=2, returns [R, R, L]
 
     Args:
         n_iterations (int): number of iterations of the dragon curve
 
     Returns:
-        str: The dragon curve Sequence
+        list: The dragon curve Sequence
     """
-    initial_sequence = R
+    sequence = [R,]
     for i in range(0, n_iterations):
-        initial_sequence = iterate(initial_sequence)
-    return initial_sequence
+        sequence.extend(next_steps(sequence))
+    return sequence
